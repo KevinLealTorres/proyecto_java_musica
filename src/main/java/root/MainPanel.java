03f8b743ca6d52c1a61f504faacc6855bfc28b4f
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-05-03 19:34:29 -0600

    código terminado

---
+++ src/main/java/root/MainPanel.java
@@ -0,0 +1,16 @@
+package root;
+import jakarta.persistence.EntityManager;
+
+import javax.swing.*;
+
+public class MainPanel extends JPanel {
+    EntityManager entityManager;
+    public MainPanel(EntityManager entityManager){
+        this.entityManager = entityManager;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(new ArtistPanel(this.entityManager));
+        add(new AlbumPanel(this.entityManager));
+//        add(new SongPanel());
+        add(new SearchPanel(this.entityManager));
+    }
+}
